b42f5e2a8f7a548710abc1d4a5f1b5ebd5dddb2d
root <root@DESKTOP-3E6F0KO.localdomain>
2020-05-11 13:55:40 -0400

    Added RNGs for food, army and money

---
+++ info.js
@@ -0,0 +1,19 @@
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+function combatScore(a, b, c){
+	return (Math.floor((a*.35) + (b*.4) + (c*.25)));
+	//a=food
+	//b=army
+	//c=money
+}
+var Yfood = randomInteger(40000, 50000), Afood = randomInteger(30000,35000), Lfood = randomInteger(30000,40000), Efood = randomInteger(20000, 30000);
+var Yarmy = randomInteger(5000, 7500), Aarmy = randomInteger(20000,25000), Larmy = randomInteger(15000,20000), Earmy = randomInteger(10000, 20000);
+let Ymoney = randomInteger(35000,50000), Amoney = randomInteger(10000,20000), Lmoney = randomInteger(15000,20000), Emoney = randomInteger(10000, 20000);
+
+var info ={
+	"info0": "Your current army size is " + Earmy + ". Your current gold is " + Emoney + ". Your current food stook is " + Efood + ".",
+	"info1": "You will gain and lose followers depending on your recent combat results. Go down to 0 and it is " + "<b>" + "GAME OVER" + "</b>",
+	"info2": "You will gain and lose gold depending on your recent combat results. Go down to 0 and it is " + "<b>" + "GAME OVER" + "</b>",
+	"info3": "You will lose 2 food stocks for every soldier u have in your army after every battle. Food stock will replenish every 7 days. Go down to 0 and it is " + "<b>" + "GAME OVER" + "</b>"
+}
