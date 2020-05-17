9a5c1017b8de884d0e38c4cf5992ea86de1c1b77
root <root@DESKTOP-3E6F0KO.localdomain>
2020-05-17 00:06:19 -0400

    Added expeditions room and more power to the user

--- rooms.js
+++ rooms.js
@@ -1,32 +1,40 @@
 var rooms = {
     "start": {
         "description": "You are in the entrance to your castle. The throne room is to the <b>north</b>\
-     and the war chambers is to the <b>west</b>",
+     and the war chambers are to the <b>west</b>. Your expedition caravan is to the <b>east</b>.",
         "directions": {
             "north": "throne_room",
-            "west": "war_chambers"
+            "west": "war_chambers",
+			"east": "expeditions"
         }
     },
     "throne_room": {
-        "description": "You arrive to a clearing, you see a lighthouse to the <b>north</b>\
-     and there is a strange smell coming from the <b>east</b>",
+        "description": info.info0 + "<br>" + info.info1 + "<br>" + info.info2 + "<br>" + info.info3 + "<br>" + info.info4,
         "directions": {
             "south": "start",
-            //"north": "lighthouse",
-            //"east": "trolls"
         }
     },
     "war_chambers": {
-        "description1": "You enter the room and begin reading the recent scouting reports",
-		"description2": "The Kingdom of Yiramm has declared attack on you. You have " + Emoney +\
-		" Meanwhile, the enemy has " + Ymoney + ".",
+        "description": "You enter the room and begin reading the recent scouting reports",
+		"war_reports": {
+			"description2": "Be prepared! The Kingdom of Yiraam has just declared attack on you! Their combat score is " + combatScore(Yfood, Yarmy, Ymoney),
+			"description3": "Your troops carry out a midnight raid. The Kingdom of Yiraam is unprepared for your attack! Their combat score is " + combatScore(Yfood, Yarmy, Ymoney),
+			"description4": "It's a sneak attack! The Hordes of Alaysa are advancing towards your position. Prepare your defenses. Their combat score is " + combatScore(Afood, Aarmy, Amoney),
+			"description5": "You've sprung a trap. This is your chance to weaken the Hordes of Alaysa. Their combat score is " + combatScore(Afood, Aarmy, Amoney),
+			"description6": "Ambush! The mighty navy of the Kingdom of Lareal are attacking your forces with all the firepower. Their combat score is " + combatScore(Lfood, Larmy, Lmoney),
+			"description7": "Now's your chance! The enemy is weak; strike back at the Kingdom of Lareal. Their combat score is " + combatScore(Lfood, Larmy, Lmoney)
+		},
         "directions": {
             "east": "start"
         }
     },
-    "trolls": {
-        "description": "You arrive to another clearing, there are some trolls roasting some mysterious meat\
-     They haven't seen you yet. What do you do?",
+    "expeditions": {
+        "description": "Decide on the expedition to send your troops to. Each expedition will reward with one of army, food or money. However, the gain of one requirement will come at the cost of another.",
+		"choices":{
+			"a": "Travel to the mages of Norwood Forest", 
+			"b": "Enlist more farmers to increase your crop production",
+			"c": "Increase the taxes in the kingdom to better finance your battles"
+		},
         "directions": {
             "west": "clearing1"
         }
