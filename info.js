4a881abebb0bfe47dc20257ecc648ecf954cb96d
root <root@DESKTOP-3E6F0KO.localdomain>
2020-05-18 16:31:56 -0400

    increased food RNG; removed fixed combat score

--- info.js
+++ info.js
@@ -2,7 +2,7 @@ function randomInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 function combatScore(a, b, c){
-	return (Math.floor((a*.35) + (b*.4) + (c*.25)));
+	return (Math.floor((a*.25) + (b*.4) + (c*.35)));
 	//a=food
 	//b=army
 	//c=money
@@ -12,12 +12,14 @@ function eventWithProbability(probability){
 	else return false;
 }
 function updateVariables_food(){
-	let Exped_food = randomInteger(500, 750);
+	let Exped_food = randomInteger(1000, 1750);
 	if(eventWithProbability(.5)){ //50% chance of expedition being successful
 		Efood = Efood + Exped_food;
+		Yfood = Yfood + Exped_food;
 	}
 	else{
 		Efood = Efood - Exped_food;
+		Yfood = Yfood - Exped_food;
 	}
 	//Efood = Efood + Exped_food;
 }
@@ -25,9 +27,11 @@ function updateVariables_army(){
 	let Exped_army = randomInteger(250,500);
 	if(eventWithProbability(.5)){ //50% chance of expedition being successful
 		Earmy = Earmy + Exped_army;
+		Yarmy = Yarmy + Exped_army;
 	}
 	else{
 		Earmy = Earmy - Exped_army;
+		Yarmy = Yarmy - Exped_army;
 	}
 	//Earmy = Earmy + Exped_army;
 }
@@ -35,20 +39,27 @@ function updateVariables_money(){
 	let Exped_money = randomInteger(250,750);
 	if(eventWithProbability(.5)){ //50% chance of expedition being successful
 		Emoney = Emoney + Exped_money;
+		Ymoney = Ymoney + Exped_money;
 	}
 	else{
 		Emoney = Emoney - Exped_money;
+		Ymoney = Ymoney - Exped_money;
 	}
 }
 
-var Yfood = randomInteger(40000, 50000), Efood = randomInteger(20000, 30000);
-var Yarmy = randomInteger(5000, 7500), Earmy = randomInteger(10000, 20000);
-let Ymoney = randomInteger(35000,50000), Emoney = randomInteger(10000, 20000);
+var Yfood = randomInteger(55000, 85000), Efood = randomInteger(50000, 65000);
+var Yarmy = randomInteger(5000, 7500), Earmy = randomInteger(15000, 25000);
+let Ymoney = randomInteger(25000,30000), Emoney = randomInteger(15000, 20000);
 
-var info ={
+var info = {
 	"info0": "Your current army size is " + Earmy + ". Your current gold is " + Emoney + ". Your current food stook is " + Efood + ".",
 	"info1": "You will gain and lose followers depending on your recent combat results. Go down to 0 and it is " + "<b>" + "GAME OVER" + "</b>",
 	"info2": "You will gain and lose gold depending on your recent combat results. Go down to 0 and it is " + "<b>" + "GAME OVER" + "</b>",
 	"info3": "You will lose 2 food stocks for every soldier u have in your army after every battle. Food stock will replenish every day. Go down to 0 and it is " + "<b>" + "GAME OVER" + "</b>",
-	"info4": "Your current combat score is " + combatScore(Efood, Earmy, Emoney) + ". Your combat score will change after every battle."
 }
+/*var strats = {
+	"a": "You have the advantage. Press forward!",
+	"b": "The enemy has the advantage. Would you like to continue your attack?",
+	"c": "Leave some soldiers behind to cover your retreat",
+	"d": "Order a full scale retreat"
+}*/
